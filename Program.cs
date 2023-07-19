98a09ab4adae5663567d5afc0fa565ceb9e83d54
MuhammadMangitov <mangitovmuhammad2256@gmail.com>
2023-07-19 19:33:46 +0500

    string uyga vazifa

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace STRING_uyga
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("ha azizlar!!");
+        }
+    }
+}
