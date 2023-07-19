0f16e2bad1460d255839e2d7b59a96fd712dbe3e
MuhammadMangitov <mangitovmuhammad2256@gmail.com>
2023-07-19 19:40:15 +0500

    task1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,35 @@
-﻿namespace STRING_uyga
+﻿using System;
+using System.Text;
+
+namespace method1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("ha azizlar!!");
+            int count = 1;
+            int kop = 1;
+            char qaysiHarf = ' ';
+            // aaacccccaavd --> c 5 marta;
+            Console.Write("Satr kiriting -> ");
+            string str = Console.ReadLine();
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i] == str[i + 1])
+                {
+                    count++;
+                    if (count > kop)
+                    {
+                        kop = count;
+                        qaysiHarf = str[i];
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            Console.WriteLine(qaysiHarf + " --> " + kop);
         }
     }
 }
