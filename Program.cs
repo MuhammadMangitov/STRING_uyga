280bdbedc234534236cb4f97882ecc97aa28f5fb
MuhammadMangitov <mangitovmuhammad2256@gmail.com>
2023-07-19 19:50:12 +0500

    TASK 3

--- Program.cs
+++ Program.cs
@@ -1,11 +1,28 @@
-﻿namespace STRING_uyga
+﻿using System;
+using System.Text;
+
+namespace method1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("ha azizlar!!");
+            string KattaSoz = "";
+            Console.Write("Satr kiritng -> ");
+            string str = Console.ReadLine();
+            string[] str1 = str.Split(' ', '`');
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (str1[i].Length > KattaSoz.Length)
+                {
+                    KattaSoz = str1[i];
+                }
+                else
+                {
+                    str1[i] = KattaSoz;
+                }
+            }
+            Console.WriteLine($"Satrda eng katta soz -> {KattaSoz}");
         }
     }
 }
