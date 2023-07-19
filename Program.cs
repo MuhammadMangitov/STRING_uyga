c935715e2d823eaa99957f10788abcc6caa08358
MuhammadMangitov <mangitovmuhammad2256@gmail.com>
2023-07-19 19:46:52 +0500

    TASK2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
-﻿namespace STRING_uyga
+﻿using System;
+using System.Text;
+
+namespace method1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("ha azizlar!!");
+            string s1 = "";
+            string s2 = "";
+            Console.Write("Ism familya otangizni ismini kiritng -> ");
+            string satr = Console.ReadLine();
+            string[] strings = satr.Split();
+            s1 += strings[0].Substring(0, 1);
+            s2 += strings[1].Substring(0, 1);
+            Console.WriteLine($"{s1}.{s2}.{strings[2]} {strings[3]}");
         }
     }
 }
